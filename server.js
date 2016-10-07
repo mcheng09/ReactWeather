df43b089bf4ceee52266c9de700aab86f44ce060
Michael Cheng <mcheng.oc@gmail.com>
2016-10-07 10:50:28 +0800

    preps repo for Heroku

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.header['x-forwarded-proto'] === 'http') {
+    next();
+  }
+  else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log("Yo Port 3000")
-})
+app.listen(PORT, function(){
+  console.log("Yo server is running on  " + PORT);
+});
