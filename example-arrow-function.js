04e0171f29d50216441b27157a66faa5399722f7
Michael Cheng <mcheng.oc@gmail.com>
2016-10-07 08:32:54 +0800

    refactors components to arrow function

--- example-arrow-function.js
+++ example-arrow-function.js
@@ -13,11 +13,31 @@ var names = ['Mike', 'Min', 'Ben', 'Matt'];
 // var returnMe = (name) => name + '!';
 // console.log(returnMe('Mike'));
 
-var person = {
-  name: 'Mike',
-  greet: function() {
-    names.forEach(function(name) {
-      console.log(this.name + ' says hi to ' + name)
-    });
-  }
-};
+// var person = {
+//   name: 'Mike',
+//   greet: function() {
+//     names.forEach((name) => {
+//       console.log(this.name + ' says hi to ' + name)
+//     });
+//   }
+// };
+//
+// person.greet();
+
+// Challenge Area
+function add(a , b) {
+  return a + b
+}
+
+// addStatement
+var addStatement = (a, b) => {
+  return a + b;
+}
+console.log(addStatement(4,7));
+
+// addExpression
+var addExpression = (a, b) => a + b;
+console.log(addExpression(3, -2));
+
+// console.log(add(1,3));
+// console.log(add(9,0));
