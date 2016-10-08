cbaeec3d4397467d5eef0988dc729500e8f8600a
Michael Cheng <mcheng.oc@gmail.com>
2016-10-08 13:56:31 -0700

    styles example page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Example = React.createClass({
 //   render: function() {
@@ -11,8 +12,16 @@ var React = require('react');
 var Example = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=WalnutCreek'>Walnut Creek, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tokyo'>Tokyo, Japan</Link>
+        </li>
+      </ol>
     </div>
   )
 };
